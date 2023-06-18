fdd8a4a1111ef97d03f9c2422ed4977145b1ca52
Andry J. Hernandez <andryj9622@gmail.com>
2023-06-18 17:35:08 -0300

    feat: Proposal analizer function

---
+++ apps/proposals/utils.py
@@ -0,0 +1,20 @@
+from .models import PROPOSAL_STATUS
+
+
+def loan_proposal_analyze(proposal_id: int) -> PROPOSAL_STATUS:
+    """
+    Analyze a given personal loan proposal
+
+    Returns if a proposal was APPROVED or DENIED based in
+    its id for test purposes
+
+    Even id will be approved and odd id will be denied
+
+    Args:
+        proposal_id: Proposal object id
+
+    Returns:
+        PROPOSAL_STATUS
+    """
+
+    return PROPOSAL_STATUS.APPROVED if proposal_id % 2 else PROPOSAL_STATUS.DENIED  # noqa

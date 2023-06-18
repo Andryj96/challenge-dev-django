815e5a92b92f8ebd22aa2df31915df77b15f1301
Andry J. Hernandez <andryj9622@gmail.com>
2023-06-18 14:55:30 -0300

    feat: Validate required and types in fields

--- apps/proposals/serializers.py
+++ apps/proposals/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from decimal import Decimal
 from apps.proposals import models
 
 
@@ -32,6 +33,47 @@ class LoanProposalSerializer(serializers.ModelSerializer):
         fields = ['uuid', 'field_values', 'status', 'analyzed_at',
                   'created_at', 'updated_at']
         read_only_fields = ['status', 'analyzed_at']
+
+    def validate(self, attrs):
+        field_values_data = attrs.get('field_values', [])
+        required_fields = models.ProposalField.objects.filter(
+            required=True).values_list('slug', flat=True)
+
+        # Validate ProposalFieldValue properties
+        for slug in required_fields:
+            # filter data
+            verify_required_slug = list(filter(
+                lambda field_data: field_data['field'].slug == slug, field_values_data  # noqa
+            ))
+            if len(verify_required_slug) == 0:
+                raise serializers.ValidationError(
+                    {'Error': [f'Field "{slug}" is required']})
+
+            elif len(verify_required_slug) > 1:
+                raise serializers.ValidationError(
+                    {'Error': [f'Duplicate field slug: {slug}']})
+
+            field_type = verify_required_slug[0]['field'].type
+            field_value = verify_required_slug[0]['value']
+
+            if field_type == models.FIELD_TYPES.NUMBER:
+                try:
+                    Decimal(field_value)
+                except Exception:
+                    raise serializers.ValidationError(
+                        {'Error': [f'Field  "{slug}" must be Decimal number']})
+
+            elif field_type == models.FIELD_TYPES.TEXT and \
+                    not isinstance(field_value, str):  # noqa
+                raise serializers.ValidationError(
+                    {'Error': [f'Field  "{slug}" must be string']})
+
+            elif field_type == models.FIELD_TYPES.CHECKBOX and \
+                    field_value not in ['True', 'False']:  # noqa
+                raise serializers.ValidationError(
+                    {'Error': [f'Field  "{slug}" must be True or False']})
+
+        return attrs
 
     def create(self, validated_data):
         field_values_data = validated_data.pop('field_values')

0607a24447e5bc45943297ced7755f1223225f9f
Andry J. Hernandez <andryj9622@gmail.com>
2023-06-19 14:34:54 -0300

    feat: Setup react ts frontend and docker image

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DynamicForm from "./components/DynamicForm";
+
+function App() {
+  return (
+    <>
+      <div className="flex text-left w-1/2 m-auto justify-center my-5 ">
+        <h1 className="text-3xl">Personal Loan Proposal</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

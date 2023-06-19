28c095884c6a00e1c8c597dd6ce698f39e540648
Andry J. Hernandez <andryj9622@gmail.com>
2023-06-19 14:36:34 -0300

    feat: Show form in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,9 @@ function App() {
       <div className="flex text-left w-1/2 m-auto justify-center my-5 ">
         <h1 className="text-3xl">Personal Loan Proposal</h1>
       </div>
+      <div className="flex text-left w-1/2 m-auto justify-center  ">
+        <DynamicForm />
+      </div>
     </>
   );
 }
